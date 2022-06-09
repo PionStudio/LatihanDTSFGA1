20d883e5654a37f27d61dadba92635d1bcd093a0
PionStudio <107085546+PionStudio@users.noreply.github.com>
2022-06-09 13:09:41 +0700

    gerakin bola & padel

---
+++ Ping Pong/Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleControl : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    [SerializeField] private KeyCode upKey;
+    [SerializeField] private KeyCode downKey;
+
+    private Rigidbody2D myRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(GetInput());
+    }
+
+    private Vector2 GetInput()
+    {
+        Vector2 movement = Vector2.zero;
+
+
+        if (Input.GetKey(upKey))
+        {
+            movement = Vector2.up * speed;
+        }
+
+        else if (Input.GetKey(downKey))
+        {
+            movement = Vector2.down * speed;
+        }
+
+        return movement; 
+    }
+
+    private void Movement(Vector2 move)
+    {
+        myRb.velocity = move;
+    }
+}
