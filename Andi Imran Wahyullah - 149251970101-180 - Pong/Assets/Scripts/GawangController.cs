0403c820a3e662dcdb240f25fef31ae7272d0ad7
PionStudio <107085546+PionStudio@users.noreply.github.com>
2022-06-09 13:15:52 +0700

    gerakin bola & padel

---
+++ Andi Imran Wahyullah - 149251970101-180 - Pong/Assets/Scripts/GawangController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GawangController : MonoBehaviour
+{
+    [SerializeField] private ScoreManager scoreManager;
+    [SerializeField] private bool gawangKiri;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ball"))
+        {
+            collision.GetComponent<BallControl>().ResetBall();
+            scoreManager.CetakGoal(gawangKiri);
+        }
+    }
+}
---
+++ Andi Imran Wahyullah - 149251970101-180 - Pong/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private Text gawangKiriText;
+    [SerializeField] private Text gawangKananText;
+
+    private int skorKiri;
+    private int skorKanan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gawangKananText.text = skorKanan.ToString();
+        gawangKiriText.text = skorKiri.ToString();
+    }
+
+    public void CetakGoal(bool kiriGawang)
+    {
+        if (kiriGawang)
+        {
+            skorKiri++;
+            return;
+        }
+
+        skorKanan++;
+    }
+}
