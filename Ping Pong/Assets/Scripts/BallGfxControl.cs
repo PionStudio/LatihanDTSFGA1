a047a7d7c804cfb6e4413db195528458200269b9
PionStudio <107085546+PionStudio@users.noreply.github.com>
2022-06-09 13:04:46 +0700

    gerakin bola & padel

---
+++ Ping Pong/Assets/Scripts/BallGfxControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGfxControl : MonoBehaviour
+{
+    [SerializeField] private GameObject parentObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D parentRb = parentObject.GetComponent<Rigidbody2D>();
+
+        //transform.LookAt(new Vector3(parentRb.velocity.x, 0f, parentRb.velocity.y));
+    }
+}
